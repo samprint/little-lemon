d31c7cb45209404eea14f6b92b9234514fac25f2
samprint <simo1print@gmail.com>
2025-04-24 11:46:03 +0900

    17 Exercise: Fetching and storing the food menu - Step 4 (working on it)
    Step 3 (Done)

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -14,9 +14,28 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
 
 class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,6 +49,20 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val itemsListMenu = httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body<MenuNetwork>()
+
+        return itemsListMenu.menu
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
+    }
+
 }
 
 @Composable
---
+++ app/src/main/java/com/example/littlelemon/Network.kt
@@ -0,0 +1,45 @@
+package com.example.littlelemon
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class MenuNetwork(
+    // add code here
+    @SerialName("menu")
+    val menu: List<MenuItemNetwork>,
+)
+
+@Serializable
+data class MenuItemNetwork(
+    // add code here
+    @SerialName("id")
+    val id: Int,
+
+    @SerialName("title")
+    val title: String,
+
+    @SerialName("description")
+    val description: String,
+
+    @SerialName("price")
+    val price: Double,
+
+    @SerialName("image")
+    val image: String,
+
+    @SerialName("category")
+    val category: String,
+
+    ) {
+    fun toMenuItemRoom() = MenuItemRoom(
+        // add code here
+        id,
+        title,
+        description,
+        price,
+        image,
+        category,
+    )
+}
