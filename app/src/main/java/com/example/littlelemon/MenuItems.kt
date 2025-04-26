d56934d14e828c07ca0723fa4677d163a0df98d0
samprint <simo1print@gmail.com>
2025-04-26 18:52:23 +0900

    24 Exercise: Fetching and storing the food menu - Step 5 (working on it)
    can display list with images, but need formatting

--- app/src/main/java/com/example/littlelemon/MenuItems.kt
+++ app/src/main/java/com/example/littlelemon/MenuItems.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -64,7 +65,7 @@ fun MenuItem( item: MenuItemRoom
         Image(
             painter = rememberAsyncImagePainter(model = item.image),
             contentDescription = item.title,
-            modifier = Modifier,
+            modifier = Modifier.size(80.dp),
 //            contentScale = ContentScale.Crop
         )
     }
