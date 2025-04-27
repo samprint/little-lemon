97e6bf47aadc6fc430fd227bc41f72cd37c5b9be
samprint <simo1print@gmail.com>
2025-04-27 12:06:54 +0900

    28 Exercise: Fetching and storing the food menu - Step 5 (Done)

--- app/src/main/java/com/example/littlelemon/MenuItems.kt
+++ app/src/main/java/com/example/littlelemon/MenuItems.kt
@@ -1,10 +1,14 @@
 package com.example.littlelemon
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,8 +22,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 
@@ -51,61 +57,81 @@ fun MenuItem( item: MenuItemRoom
 ){
     Column (
         modifier = Modifier
-            .padding(start = 15.dp, end = 15.dp)
+            .padding(
+                start = 15.dp,
+                end = 15.dp,
+                top = 5.dp,
+            )
     ){
-        Text( // Title
-            item.title,
-            color = Color.Black,
-            modifier = Modifier
-                .padding(top = 10.dp, bottom = 5.dp)
-            ,
-            fontFamily = KarlaRegularFont,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
-        )
-
         Row (
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp),
             verticalAlignment = Alignment.CenterVertically,
         ){
             Column (
                 modifier = Modifier.weight(0.8f)
             ){
+                Text( // Title
+                    item.title,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .padding(top = 10.dp, bottom = 5.dp)
+                    ,
+                    fontFamily = KarlaRegularFont,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 20.sp,
+                    letterSpacing = (-0.03).em
+                )
+
                 Text( // Description
                     item.description,
                     color = Color.Gray,
                     fontFamily = KarlaRegularFont,
-//                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp
-
+                    fontSize = 16.sp,
+                    softWrap = true,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    letterSpacing = (-0.03).em,
+                    modifier = Modifier
+                        .padding(
+                            bottom = 10.dp,
+                            end = 10.dp
+                        )
                 )
                 Text(
                     "$" + "%.2f".format(item.price),
                     color = Color.Black,
                     fontFamily = KarlaRegularFont,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp,
+                    fontSize = 20.sp,
+                    letterSpacing = (-0.03).em,
                     modifier = Modifier
-                        .padding(
-                            top = 10.dp,
-                            bottom = 10.dp,
-                        )
                         ,
-
                 )
             }
-            Column {
+            Column (
+                modifier = Modifier
+                    .weight(0.2f)
+                    .fillMaxSize()
+                    ,
+            ){
+                Spacer(
+                    modifier = Modifier
+                        .height(25.dp)
+                )
                 Image(
                     painter = rememberAsyncImagePainter(model = item.image),
                     contentDescription = item.title,
-                    modifier = Modifier.size(80.dp),
+                    modifier = Modifier
+                        .size(80.dp)
+                        ,
                     contentScale = ContentScale.Crop
                 )
             }
         }
         HorizontalDivider(
             color = Color.LightGray,
-            thickness = 1.dp
+            thickness = 0.5.dp
         )
     }
 }
